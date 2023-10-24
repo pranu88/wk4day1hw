bee06f4d10d3514fd6cceb40821189cf54563e6b
pranu88 <pranusha.pothugunta@gmail.com>
2023-10-24 19:16:36 -0400

    created timmys story

--- classesAndFactories.js
+++ classesAndFactories.js
@@ -1,8 +1,8 @@
 class Hamster {
     owner = " "
     price = 15
-    constructor(name){
-        this.name = name
+    constructor(hname){
+        this.hname = hname
     }
     wheelRun(){
         console.log("squeak squeak")
@@ -14,4 +14,73 @@ class Hamster {
         return this.price
     }
 }
+
+class Person {
+    constructor(name){
+        this.name = name
+    }
+    age = 0
+    height = 0
+    weight = 0
+    mood = 0
+    hamsters = []
+    bankAccount = 0
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.age
+    }
+    getWeight(){
+        return this.weight
+    }
+    greet(){
+        console.log(`Hello ${this.name}!`)
+    }
+    eat(){
+        this.weight++
+        this.mood++
+    }
+    exercise(){
+        this.weight--
+    }
+    ageUp(){
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
+    }
+    buyHamster(){        
+        this.hamsters.push(gus)
+        this.mood += 10
+        this.bankAccount = this.bankAccount - gus.getPrice()
+        return this.bankAccount
+    }
+}
+// creating object for Hampster class
+let gus = new Hamster("Gus")
+// creating object for Person class
+const timmy = new Person('Timmy')
+timmy.age =5
+timmy.eat()
+timmy.eat()
+timmy.eat()
+timmy.eat()
+timmy.eat()
+timmy.exercise()
+timmy.exercise()
+timmy.exercise()
+timmy.exercise()
+timmy.exercise()
+timmy.age =9
+gus.owner = "Timmy"
+timmy.buyHamster()
+timmy.age = 15
+timmy.eat()
+timmy.eat()
+console.log(timmy)
+timmy.exercise()
+timmy.exercise()
+console.log(timmy)
 
